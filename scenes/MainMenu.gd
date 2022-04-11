c3778088e42d82eee64cbabd7208b8eeb0fc41a4
Millaray Valdivia Saldias <mws.jbs@gmail.com>
2022-04-11 00:48:42 -0400

    Main Menu to Hotel Scene is working

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -18,7 +18,7 @@ func _ready():
 
 
 func _on_play_pressed():
-	get_tree().change_scene("res://scenes/Main.tscn")
+	get_tree().change_scene("res://Escena/HotelV1.tscn")
 	
 func _on_exit_pressed():
 	get_tree().quit() 
