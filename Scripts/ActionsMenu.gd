c09e48bfdf102f8288115bb638cd3892f824c109
Millaray Valdivia Saldias <mws.jbs@gmail.com>
2022-05-08 16:39:57 -0400

    Avance en el inventario

--- Scripts/ActionsMenu.gd
+++ Scripts/ActionsMenu.gd
@@ -1,9 +1,18 @@
 extends MarginContainer
 
+onready var grid_container = $GridContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false # Replace with function body.
+	var file = File.new()
+	file.open("res://Data/inventory.json", File.READ)
+	var items = JSON.parse(file.get_as_text()).result
+	file.close()
+	
+	for slotIndex in items.keys():
+		var slot: Slot = grid_container.get_child(int(slotIndex))
+		slot.set_item(items[slotIndex].item)
 
 func _input(event):
 	if event.is_action_pressed("pause") and not event.is_echo():
--- Scripts/slot.gd
+++ Scripts/slot.gd
@@ -1,10 +1,25 @@
 tool
+class_name Slot
 extends TextureRect
 
 export(Texture) var empty_texture
 export(Texture) var full_texture
+
+var Item = preload("res://Escena/Item.tscn")
+
 export var empty = true setget set_empty
+var _item setget set_item_icon
 
 func set_empty(value):
 	empty = value
 	texture = empty_texture if empty else full_texture
+	
+func set_item(item):
+	self._item = item
+	
+func set_item_icon(value):
+	_item = value
+	var i = Item.instance()
+	i.texture = load("res://assets/inventory/waterdrop.png")
+	add_child(i)
+	set_empty(false)
