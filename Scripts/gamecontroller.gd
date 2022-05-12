c48e1991bae6e59453c9fbab678c33719bf601fe
Millaray Valdivia Saldias <mws.jbs@gmail.com>
2022-05-11 21:52:09 -0400

    Inquilino como recurso agregado

--- Scripts/gamecontroller.gd
+++ Scripts/gamecontroller.gd
@@ -1,9 +1,3 @@
 extends Node
 var action_counter=0
 var _legend = "Esta casilla está vacía."
-onready var inventory = get_node("../ActionsMenu")
-func set_legend(value):
-	_legend = str(value)
-	inventory.set_legend(_legend)
-	
-	
