9fd86429d61cd16363475f2f0e67e0cda9bc18ee
lepinn <aabbcczz@live.cl>
2022-06-10 12:59:51 -0400

    Corazones listos

--- Scripts/hearts.gd
+++ Scripts/hearts.gd
@@ -2,23 +2,13 @@ extends HBoxContainer
 
 var heart_full = preload("res://assets/heart.png")
 var heart_empty = preload("res://assets/heartempty.png")
-enum TYPES {full,deplete} # enumera 1 Full, 2 deplete
-export (TYPES) var type = TYPES.full
 
-func update_health(value): #cambiar la función de entrada
-	match type:
-		TYPES.full:
-			update_full(value)#cambiar a solo 1 update_heart con value
-		TYPES.deplete:
-			update_deplete(value)
-
-
-func update_full(value):
-	for i in self.get_child_count():
-		if i < value:
-			get_child(i).show()
-		else:
-			get_child(i).show()
+#func update_full(value):
+#	for i in self.get_child_count():
+#		if i < value:
+#			get_child(i).show()
+#		else:
+#			get_child(i).show()
 			
 func update_deplete(value):
 	for i in self.get_child_count():
@@ -27,3 +17,7 @@ func update_deplete(value):
 		else:
 			get_child(i).texture =heart_empty
 	
+
+
+
+
