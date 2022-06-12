5ed785a943816275a10af5f8e0fa7e9562720e11
Millaray Valdivia Saldias <mws.jbs@gmail.com>
2022-06-11 23:48:18 -0400

    Menú de pausa añadido, problema: se abren dos cuando se apreta espacio

---
+++ Scripts/Pause.gd
@@ -0,0 +1,36 @@
+extends Control
+
+onready var resume = $PanelContainer/VBoxContainer/Resume
+onready var exit = $PanelContainer/VBoxContainer/Exit
+onready var menu = $PanelContainer/VBoxContainer/MainMenu
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	resume.connect("pressed", self, "_on_resume_pressed")
+	exit.connect("pressed", self, "_on_exit_pressed")
+	menu.connect("pressed", self, "_on_menu_pressed")
+	visible = false
+
+func _input(event):
+	if event.is_action_pressed("pause") and not event.is_echo():
+		print("pause")
+		get_tree().paused = true
+		self.visible = true
+		
+func _on_resume_pressed():
+	print("resume")
+	get_tree().paused = false
+	self.visible = false
+	
+func _on_exit_pressed():
+	get_tree().quit() 
+
+func _on_menu_pressed():
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
