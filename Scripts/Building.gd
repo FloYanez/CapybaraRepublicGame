a044b5c7a1a25d5a5616b4a437c6c310ab5e1da9
Millaray Valdivia Saldias <mws.jbs@gmail.com>
2022-06-24 12:52:26 -0400

    Animacion dia y noche conectada a las acciones

--- Scripts/Building.gd
+++ Scripts/Building.gd
@@ -8,6 +8,8 @@ var accept_input= true
 
 func _ready():
 	InventoryButton.connect("pressed", self, "on_inventory_pressed")
+	Gamecontroller.background = backgroundcycle
+	Gamecontroller.building = self
 	
 func _input(event):
 	if not accept_input:#si no hay input no pasa nada
@@ -16,36 +18,6 @@ func _input(event):
 	if event.is_action_pressed("pause") and not event.is_echo():
 		get_tree().paused = true
 		pause.visible = true
-		
-	elif event.is_action_pressed('w'):
-		accept_input= false
-		Gamecontroller.action_counter +=1
-		
-		if Gamecontroller.action_counter ==2:
-			if day:
-				backgroundcycle.play("Day")
-			else:
-				backgroundcycle.play_backwards("Day")
-			Gamecontroller.action_counter = 0 
-			day = not day
-			yield(backgroundcycle,"animation_finished")#espera la ejecución de código
-		accept_input=true
-		
-func _on_action_passed():
-	if not accept_input:#si no hay input no pasa nada
-		return
-	print("action_counter: ", Gamecontroller.action_counter)
-	print("cambio de escena")
-	accept_input= false
-	if day:
-		print("dia a noche")
-		backgroundcycle.play("Day")
-	else:
-		print("noche a dia")
-		backgroundcycle.play_backwards("Day") 
-	day = not day
-	yield(backgroundcycle,"animation_finished")#espera la ejecución de código
-	accept_input=true
 
 func on_inventory_pressed():
 	inventory.visible = true
--- Scripts/gamecontroller.gd
+++ Scripts/gamecontroller.gd
@@ -1,20 +1,19 @@
 extends Node
 var action_counter=0
 var current_action_count = 0
-var on_action = false
 var _current_action setget set_current_action
 var _legend = "Esta casilla está vacía."
 var tenants_apartments = []
+var day = true
+var background = null
+var building = null
 
 func set_current_action(action):
 	_current_action = action
-	if on_action == false:
-		action_counter += 1
 	while tenants_apartments.size() > 6:
 		tenants_apartments.pop_front()
 	for i in tenants_apartments:
 		i.apply_button.visible = true
-	on_action = true
 		
 func use_action():
 	current_action_count +=1
@@ -24,7 +23,18 @@ func use_action():
 		for i in tenants_apartments:
 			i.apply_button.visible = false
 		current_action_count = 0
-		on_action = false
-		if action_counter ==  2:
-			Building._on_action_passed()
-			action_counter = 0
+	increase_action_counter()
+			
+func increase_action_counter():
+	action_counter +=1
+	if action_counter ==2:
+		if day:
+			background.play("Day")
+		else:
+			background.play_backwards("Day")
+		action_counter = 0 
+		day = not day
+		yield(background,"animation_finished")#espera la ejecución de código
+
+
+
