aee7ed99e8820a27885bbc04bf031f5a6a3adc52
Millaray Valdivia Saldias <mws.jbs@gmail.com>
2022-07-06 17:19:58 -0400

    Barra de corazones se actualiza cuando se hace una accion

--- Scripts/genericApartment.gd
+++ Scripts/genericApartment.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var tenant_container = $Tenant
 onready var fondo = $TextureRect
 onready var apply_button = $ApplyButton
-
+onready var health_bar = $Hearts
 export(Resource) var tenant
 
 var heart_count
@@ -16,6 +16,12 @@ func _ready():
 
 func on_apply():
 	if Gamecontroller._current_action != null:
-		tenant.current_likes += tenant.likes[Gamecontroller._current_action]
+		var new_count = tenant.current_likes + tenant.likes[Gamecontroller._current_action]
+		if new_count < 0:
+			new_count = 0
+		elif new_count > 6:
+			new_count = 6
+		tenant.current_likes = new_count
+		health_bar.update_count(new_count)
 		Gamecontroller.use_action()
 
--- Scripts/health_bar.gd
+++ Scripts/health_bar.gd
@@ -3,18 +3,18 @@ onready var health_bar = $hearts
 var heart_count
 var max_health
 func _ready():
-	heart_count = 6
 	max_health = heart_count
-	update_count()
+	update_count(6)
 	
-func update_count():
+func update_count(value):
+	heart_count = value
 	health_bar.update_deplete(heart_count)
-
+	
 #Funcion a cambiar para que funcione con el panel, ademas que que cuanto suma dependa de los likes y displikes
-func _physics_process(delta):
-	if Input.is_action_just_pressed("minus") and heart_count > 0: #and just_pressed:
-		heart_count -=1
-		update_count()
-	if Input.is_action_just_pressed("add") and heart_count < max_health: #and just_pressed:
-		heart_count +=1
-		update_count()
+#func _physics_process(delta):
+#	if Input.is_action_just_pressed("minus") and heart_count > 0: #and just_pressed:
+#		heart_count -=1
+#		update_count()
+#	if Input.is_action_just_pressed("add") and heart_count < max_health: #and just_pressed:
+#		heart_count +=1
+#		update_count()
